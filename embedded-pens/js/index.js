2ed63242e714b74e59c5aad8ab297861f63721ab
Maria Dubyaga <kraftaa@gmail.com>
2018-03-05 10:06:28 -0500

    Add files via upload

---
+++ embedded-pens/js/index.js
@@ -0,0 +1,11 @@
+// //for console
+// var realConsoleLog = console.log;
+//     console.log = function () {
+//         var message = [].join.call(arguments, " ");
+//         // Display the message somewhere... (jQuery example)
+//         $(".output").text(message);
+//         realConsoleLog.apply(console, arguments);
+//     };
+
+
+// console.log("ho ho");
